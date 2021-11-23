ecbd65096e51b0a2eaa5df63138231e2a6b6e92f
Elias Goss St <31922893+gossjsstudent2017@users.noreply.github.com>
2021-11-23 16:23:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ Router
 
 app
   .use('/', Router)
-  .get('/login', (req, res) => res.set(CORS).send('admin'))
+  .get('/login', (req, res) => res.set(CORS).send('gandalf'))
   .use(({ res: r }) => r.status(404).send('Пока нет!'))
   .use((e, r, rs, n) => rs.status(500).send(`Ошибка: ${e}`))
   .set('x-powered-by', false);
